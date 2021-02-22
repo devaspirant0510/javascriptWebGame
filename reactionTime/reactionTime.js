44228bb6d9317e22a7d31aa67dfae15e062df628
Seung <68223593+devaspirant0510@users.noreply.github.com>
2021-02-22 23:53:37 +0900

    구현완료

--- reactionTime/reactionTime.js
+++ reactionTime/reactionTime.js
@@ -1,52 +1,46 @@
-/*
 let mainScreen = document.querySelector("#main-screen");
 let timeout;
 let startTime;
 let endTime;
-mainScreen.addEventListener("click",()=>{
+
+function changeScreen(before, now) {
+    mainScreen.classList.remove(before);
+    mainScreen.classList.add(now);
+}
+
+mainScreen.addEventListener("click", () => {
     console.log(mainScreen.className)
-    if(mainScreen.classList.contains("init")){
-
-        mainScreen.classList.remove("init");
-        mainScreen.classList.add("wait");
-        let time = Math.random()*5000+1000;
-        timeout = setTimeout(()=>{
-            startTime = new Date();
-            mainScreen.click();
-        },time);
-        if(!startTime){
-            clearTimeout(timeout)
-        }
-        else{
-
-            mainScreen.textContent = "초록색이 되면 클릭하세요"
-        }
-
-    }
-    else if (mainScreen.classList.contains("wait")){
-        mainScreen.classList.remove("wait");
-        mainScreen.classList.add("stop");
-        mainScreen.textContent = "클릭하세요"
+    // 초록색 화면 클릭해야되는 부분
+    if (mainScreen.className === "wait") {
+        console.log("stop");
+        mainScreen.textContent = "클릭하세요";
+        changeScreen("wait", "stop");
 
 
     }
-    else if (mainScreen.classList.contains("stop")){
+    // 파란색 화면 나의 반응속도 보여주고 게임을 시작함
+    else if (mainScreen.className === "stop") {
+        console.log("init");
         endTime = new Date();
         console.log(endTime-startTime);
-        mainScreen.textContent = "너의 기록 "+(endTime-startTime)+"s\n클릭해서 시작하세요";
-        mainScreen.classList.remove("stop");
-        mainScreen.classList.add("init");
+        mainScreen.textContent = "클릭하여 시작하세요"
+        changeScreen("stop", "init");
+
+
+    }
+    // 빨강색 화면 일정시간이 지난후 화면 전환
+    else if (mainScreen.className === "init") {
+        console.log("wait");
+        mainScreen.textContent = "초록색이 되면 클릭하세요";
+        changeScreen("init", "wait");
+        let randTime = Math.random()*4500+1500;
+        timeout = setTimeout(()=>{
+            mainScreen.click();
+            startTime = new Date();
+        },randTime);
+
+
     }
 });
 
 
-*/
-let a;
-console.log("d");
-setTimeout(()=>{
-    a = 1;
-    console.log("3");
-    console.log(a);
-},1000)
-console.log(a);
-console.log("1");
